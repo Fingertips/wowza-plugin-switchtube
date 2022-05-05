b7cbddc54726242ab204dbc4eccb7b2d23a4339d
Manfred Stienstra <manfred@fngtps.com>
2022-05-05 16:37:29 +0200

    Add error handling and retries for callbacks.

--- src/com/fngtps/wowza/tube/Callbacks.java
+++ src/com/fngtps/wowza/tube/Callbacks.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -24,61 +25,93 @@ import com.wowza.wms.module.IModuleOnConnect;
 import com.wowza.wms.module.ModuleBase;
 import com.wowza.wms.request.RequestFunction;
 
+// Sends callbacks to SWITCHtube when RTMP streams connect for authorization, configuration, and state updates. 
 public class Callbacks extends ModuleBase implements IModuleOnConnect {
-	private Reporter reporter;
-	
+	// Raised when the callback failed due to connection or configuration issues.
+	public class CallbackFailed extends Exception {
+		private static final long serialVersionUID = 4512390269759546454L;
+
+		public CallbackFailed(String errorMessage) {
+			super(errorMessage);
+		}
+	}
+
+	// Raised when the callback failed because the server returned a forbidden
+	// result.
+	public class CallbackForbidden extends Exception {
+		private static final long serialVersionUID = 1815794114707181170L;
+
+		public CallbackForbidden(String errorMessage) {
+			super(errorMessage);
+		}
+	}
+
+	// Details sent by SWITCHtube about a channel's stream.
 	public class Stream {
+		// Name of the stream, such that the playlist URL in Wowza does not contain a
+		// stream secret.
 		private String name;
+
 		Stream() {
 		}
 	}
-	
+
+	// Callback details sent to SWITCHtube.
 	public class Event {
+		@SuppressWarnings("unused")
 		private String secret;
+		@SuppressWarnings("unused")
 		private String status;
+
 		Event() {
 		}
 	}
-	
+
+	// Utility class to compute a signature for callbacks.
 	public class Signature {
-		
+
 		private String serialized;
-		
+
 		public Signature(String secret, String newt, String payload) {
+			// Newt is a one-use token to add some entropy to the signature. The secret is a
+			// shared secret between Wowza and SWITCHtube.
 			this.serialized = newt + "\n" + secret + "\n" + payload + "\n";
 		}
 
 		public String toString() {
 			try {
-				return DatatypeConverter.printHexBinary(getHash());
+				return DatatypeConverter.printHexBinary(getHash()).toLowerCase();
 			} catch (NoSuchAlgorithmException e) {
 				e.printStackTrace();
 				return "";
 			}
 		}
-		
+
 		private byte[] getHash() throws NoSuchAlgorithmException {
 			MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
 			return messageDigest.digest(this.serialized.getBytes(StandardCharsets.UTF_8));
 		}
 
 	}
-	
+
+	// Client class to send callbacks to SWITCHtube.
 	public class Reporter {
-		
-		private String callbackUrl;
+
+		private URL callbackUrl;
 		private String callbackSecret;
 
-		public Reporter(String callbackUrl, String callbackSecret) {
-			this.callbackUrl = callbackUrl;
+		public Reporter(String callbackUrl, String callbackSecret) throws MalformedURLException {
+			this.callbackUrl = new URL(callbackUrl);
 			this.callbackSecret = callbackSecret;
 		}
-		
-		public String getCallbackUrl() {
+
+		public URL getCallbackUrl() {
 			return callbackUrl;
-		} 
-		
-		public String performCallback(String streamSecret, String status) throws MalformedURLException {
+		}
+
+		// Send a callback to SWITCHtube for a stream authenticated by the stream
+		// secret. Returns a stream name suggested by SWITCHtube.
+		public String performCallback(String streamSecret, String status) throws CallbackFailed, CallbackForbidden {
 			Gson gson = new Gson();
 
 			Event event = new Event();
@@ -87,38 +120,79 @@ public class Callbacks extends ModuleBase implements IModuleOnConnect {
 			String payload = gson.toJson(event);
 
 			String newt = generateNewt();
-			Signature signature = new Signature(this.callbackSecret, newt, payload);
+			Signature signature = new Signature(callbackSecret, newt, payload);
+
+			HttpURLConnection connection;
+			try {
+				connection = (HttpURLConnection) callbackUrl.openConnection();
+			} catch (IOException e) {
+				throw new CallbackFailed(
+						"Failed to open connection to " + callbackUrl + ": " + e.getLocalizedMessage());
+			}
+
+			// Raises a protocol exception because the request method can technically be
+			// dynamic. In our case that would be a compile-time issue.
+			try {
+				connection.setRequestMethod("POST");
+			} catch (ProtocolException e) {
+				throw new CallbackFailed("HttpURLConnection can't POST requests: " + e.getLocalizedMessage());
+			}
+
+			connection.setDoOutput(true);
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setRequestProperty("Accept", "application/json");
+			connection.setRequestProperty("X-Newt", newt);
+			connection.setRequestProperty("X-Signature", signature.toString());
 
 			try {
-				URL url = new URL(this.callbackUrl);
-				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-				connection.setDoOutput(true);
-				connection.setDoInput(true);
-				connection.setRequestMethod("POST");
-				connection.setRequestProperty("Content-Type", "application/json");
-				connection.setRequestProperty("Accept", "application/json");
-				connection.setRequestProperty("X-Newt", newt);
-				connection.setRequestProperty("X-Signature", signature.toString().toLowerCase());
-
 				OutputStream requestStream = connection.getOutputStream();
 				requestStream.write(payload.getBytes("UTF-8"));
 				requestStream.close();
-				
+			} catch (java.io.IOException e) {
+				throw new CallbackFailed("Failed to set payload for the callback request: " + e.getLocalizedMessage());
+			}
+
+			try {
+				int responseCode = connection.getResponseCode();
+				if (responseCode == 403) {
+					throw new CallbackForbidden("Callback request returned a forbidden status code.");
+				}
+			} catch (IOException e) {
+				throw new CallbackFailed("Failed to perform callback request: " + e.getLocalizedMessage());
+			}
+
+			try {
 				String json = new BufferedReader(
-					      new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))
-					        .lines()
-					        .collect(Collectors.joining("\n"));
+						new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8)).lines()
+						.collect(Collectors.joining("\n"));
 				try {
 					Stream stream = gson.fromJson(json, Stream.class);
 					return stream.name;
 				} catch (JsonSyntaxException e) {
 					System.out.println(json);
-					e.printStackTrace();
-					return "";
+					throw new CallbackFailed("Failed to parse JSON response: " + e.getLocalizedMessage());
 				}
 			} catch (IOException e) {
-				e.printStackTrace();
-				return "";
+				throw new CallbackFailed("Failed to read callback response body: " + e.getLocalizedMessage());
+			}
+		}
+
+		public String performCallbackWithRetries(String streamSecret, String status)
+				throws CallbackFailed, CallbackForbidden {
+			int retries = 2;
+			while (true) {
+				try {
+					return performCallback(streamSecret, status);
+				} catch (CallbackFailed e) {
+					System.err.println(e.getLocalizedMessage());
+					if (--retries < 0)
+						throw e;
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException ex) {
+						ex.printStackTrace();
+					}
+				}
 			}
 		}
 
@@ -126,41 +200,77 @@ public class Callbacks extends ModuleBase implements IModuleOnConnect {
 			SecureRandom random = new SecureRandom();
 			return DatatypeConverter.printHexBinary(random.generateSeed(6));
 		}
-
 	}
-	
+
+	private Reporter reporter;
+
 	public void onAppStart(IApplicationInstance appInstance) {
-		this.reporter = new Reporter(
-			appInstance.getProperties().getPropertyStr("SWITCHtubeCallbackUrl"),
-			appInstance.getProperties().getPropertyStr("SWITCHtubeCallbackSecret")
-		);
-		getLogger().info("Using " + reporter.getCallbackUrl() + " to send callbacks to SWITCHtube.");
+		String callbackUrl = appInstance.getProperties().getPropertyStr("SWITCHtubeCallbackUrl");
+		try {
+			this.reporter = new Reporter(callbackUrl,
+					appInstance.getProperties().getPropertyStr("SWITCHtubeCallbackSecret"));
+			getLogger().info("Using " + reporter.getCallbackUrl() + " to send callbacks to SWITCHtube.");
+		} catch (MalformedURLException e) {
+			getLogger().info("The configured URL " + callbackUrl + " is malformed, callbacks are disabled. "
+					+ e.getLocalizedMessage());
+		}
 	}
 
 	@Override
 	public void onConnect(IClient client, RequestFunction function, AMFDataList params) {
-		String streamSecret = client.getQueryStr();
-		try {
-			reporter.performCallback(streamSecret, "connected");
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-			client.rejectConnection("Failed to authorize connection because callback failed.");
-		}	
+		if (this.reporter == null) {
+			getLogger().info("Rejected connection because the callback is not configured properly.");
+			client.rejectConnection();
+		} else {
+			// Make sure the stream secret is value during connect because it's the only
+			// time we can reject the connection.
+			String streamSecret = client.getQueryStr();
+			try {
+				String streamName = reporter.performCallbackWithRetries(streamSecret, "connected");
+				if (streamName.isEmpty()) {
+					getLogger().info("Connection rejected because stream name is emtpy.");
+					client.rejectConnection();
+				} else {
+					getLogger().info("Allowing connection for stream with name: " + streamName);
+				}
+			} catch (CallbackFailed e) {
+				getLogger().info("Connection rejected because callback failed: " + e.getLocalizedMessage());
+				client.rejectConnection();
+			} catch (CallbackForbidden e) {
+				getLogger().info("Connection rejected because callback was forbidden: " + e.getLocalizedMessage());
+				client.rejectConnection();
+			}
+		}
 	}
 
 	public void publish(IClient client, RequestFunction function, AMFDataList params) {
+		if (this.reporter == null)
+			return;
+
+		// Force the stream name to the name suggested by SWITCHtube so a broadcaster
+		// can't create streams with random names.
 		String streamSecret = client.getQueryStr();
 		try {
-			String streamName = reporter.performCallback(streamSecret, "published");
-			getLogger().info("Callback suggests stream name " + streamName);
-			params.set(PARAM1, streamName);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-			client.rejectConnection("Failed set steam name because callback failed.");
+
+			String streamName = reporter.performCallbackWithRetries(streamSecret, "published");
+			if (streamName.isEmpty()) {
+				getLogger().info("Stop the stream because the stream name is empty.");
+				client.rejectConnection();
+			} else {
+				getLogger().info("Using stream name: " + streamName);
+				params.set(PARAM1, streamName);
+			}
+		} catch (CallbackFailed e) {
+			getLogger().info("Stream stopped because callback failed: " + e.getLocalizedMessage());
+			client.rejectConnection();
+		} catch (CallbackForbidden e) {
+			getLogger().info("Stream stopped because callback was forbidden: " + e.getLocalizedMessage());
+			client.rejectConnection();
 		}
+
 		invokePrevious(client, function, params);
 	}
-	
+
 	@Override
 	public void onConnectAccept(IClient client) {
 	}
@@ -171,6 +281,16 @@ public class Callbacks extends ModuleBase implements IModuleOnConnect {
 
 	@Override
 	public void onDisconnect(IClient client) {
+		if (this.reporter == null)
+			return;
+
+		// Let SWITCHtube know the stream disconnected.
+		String streamSecret = client.getQueryStr();
+		try {
+			reporter.performCallbackWithRetries(streamSecret, "stopped");
+		} catch (CallbackFailed | CallbackForbidden e) {
+			getLogger().info("Failed to report disconnection: " + e.getLocalizedMessage());
+		}
 	}
 
 }
